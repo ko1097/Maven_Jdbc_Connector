7d805447b8fc9aecffe693b7632e66a7d12f27b1
ko1097 <67546597+ko1097@users.noreply.github.com>
2025-09-16 15:27:45 +0530

    Update App.java

--- myapp/src/main/java/com/example/App.java
+++ myapp/src/main/java/com/example/App.java
@@ -16,24 +16,24 @@ public class App
 {
     public static void main( String[] args )
     {
-        Random rand = new Random();
-        int a = rand.nextInt(50) + 1;         // 1-50
-        int b = rand.nextInt(10) + 1; 
-        Calculator calc = new Calculator();
-        int addition_result = calc.add(a, b);
-        int multiplication_result = calc.multiply(a, b);
-        int subrtaction_result = calc.subrtact(a, b);
-        int division_result = calc.division(a, b);
-        System.out.println( addition_result +" ==> addition_result" );
-        System.out.println( subrtaction_result +" ==> subrtaction_result" );
-        System.out.println( multiplication_result + " ==> multiplication_result");
-        System.out.println( division_result + " ==> division_result");
-        SpringApplication.run(App.class, args);
+        // Random rand = new Random();
+        // int a = rand.nextInt(50) + 1;         // 1-50
+        // int b = rand.nextInt(10) + 1; 
+        // Calculator calc = new Calculator();
+        // int addition_result = calc.add(a, b);
+        // int multiplication_result = calc.multiply(a, b);
+        // int subrtaction_result = calc.subrtact(a, b);
+        // int division_result = calc.division(a, b);
+        // System.out.println( addition_result +" ==> addition_result" );
+        // System.out.println( subrtaction_result +" ==> subrtaction_result" );
+        // System.out.println( multiplication_result + " ==> multiplication_result");
+        // System.out.println( division_result + " ==> division_result");
+        // SpringApplication.run(App.class, args);
 
-        // NumberGame game = new NumberGame();
-        // game.letStart();
-        // StudentGradeCalculator gradeCalc = new StudentGradeCalculator();
-        // gradeCalc.gradeCalculator();
+        // // NumberGame game = new NumberGame();
+        // // game.letStart();
+        // // StudentGradeCalculator gradeCalc = new StudentGradeCalculator();
+        // // gradeCalc.gradeCalculator();
         }
       @Bean
         public CommandLineRunner run(FetchRecord fetchRecord) {
@@ -43,3 +43,4 @@ public class App
         }
 
 }
+
